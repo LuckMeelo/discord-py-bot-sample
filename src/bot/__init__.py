b5123be2a8f1adf701f9a2099d68b40625bb10ba
LuckMeelo <charmeel.vodouhe@epitech.eu>
2023-08-24 16:57:20 +0100

    feature(bot): add a logger
    Closes #4

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,6 +1,8 @@
+import platform, os
 import discord
 from utils.env_loader import EnvLoader
 from utils.config_loader import ConfigLoader
+from utils.logger import build_logger
 from discord.ext import commands, tasks
 
 env = EnvLoader()
@@ -15,9 +17,17 @@ intents.message_content = True
 class Bot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=config.get('prefix'), intents=intents)
+        self.logger = build_logger(name=config.get('bot_name'), logfilename=config.get('log_filename'))
 
     async def on_ready(self):
-        print(f'Logged in as {self.user.name} (ID: {self.user.id})')
+        '''
+        Print general infos about the bot and the platform its running on.
+        '''
+        self.logger.info(f"Logged in as {self.user.name}")
+        self.logger.info(f"discord.py API version: {discord.__version__}")
+        self.logger.info(f"Python version: {platform.python_version()}")
+        self.logger.info(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+        self.logger.info("-------------------")
 
     async def on_message(self, message):
         if message.author == self.user:
